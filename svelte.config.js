ad632a7f32357d5c9b72098e4e14faa63a40479a
Adam Bowman <adamwbowman@me.com>
2025-03-18 00:35:49 -0500

    svelte and vercel config fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,33 @@
 import adapter from '@sveltejs/adapter-auto';
 
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+	  adapter: adapter({
+		// if true, will deploy the app using edge functions
+		// (https://vercel.com/docs/concepts/functions/edge-functions)
+		// rather than serverless functions
+		edge: false,
+		
+		// specify external dependencies that should be bundled
+		external: ['mongodb'],
+		
+		// automatically split routes into smaller chunks
+		split: false
+	  })
 	}
-};
-
-export default config;
+  };
+  
+  export default config;
